49cd4250a1ae9ec17214911ebf6474497df3647a
dmmop <davidmtn@gmail.com>
2020-03-29 15:02:10 +0200

    Test files

---
+++ release.sh
@@ -0,0 +1,26 @@
+set -ex
+
+# docker-hub username
+USERNAME=dmmop
+# Image name
+IMAGE=calibre-importer
+
+docker build -t $USERNAME/$IMAGE:latest .
+
+# ensure we're up to date
+git pull
+# bump version
+version=`cat VERSION`
+echo "version: $version"
+# run build
+./build.sh
+# tag it
+git add -A
+git commit -m "version $version"
+git tag -a "$version" -m "version $version"
+git push
+git push --tags
+docker tag $USERNAME/$IMAGE:latest $USERNAME/$IMAGE:$version
+# push it
+docker push $USERNAME/$IMAGE:latest
+docker push $USERNAME/$IMAGE:$version
